f1cc5bc5262f08e2962006582dc6de80f4439c1a
ikram3031 <mdikram.3031@gmail.com>
2022-08-18 14:34:36 +0600

    Order Completed

--- models/Order.js
+++ models/Order.js
@@ -1,54 +1,55 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const SingleCartItemSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    Image: { type: String, required: true },
-    Price: { type: Number, required: true },
-    amount: { type: Number, required: true },
-    product:{
-        type: mongoose.Schema.ObjectId,
-        ref: 'Product',
-        required: true,
-    }
+const SingleOrderItemSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  product: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
 });
 
-const OrderSchema = mongoose.Schema({
+const OrderSchema = mongoose.Schema(
+  {
     tax: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     shippingFee: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     subtotal: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     total: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
-    cartItems: [SingleCartItemSchema],
-    status:{
-        type: String,
-        enum: ['pending', 'failed', 'paid', 'delivered', 'canceled'],
-        default: 'pending',
+    orderItems: [SingleOrderItemSchema],
+    status: {
+      type: String,
+      enum: ['pending', 'failed', 'paid', 'delivered', 'canceled'],
+      default: 'pending',
     },
-    user:{
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: true,
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
     },
-    clientSecret:{
-        type: String,
-        required: true,
+    clientSecret: {
+      type: String,
+      required: true,
     },
-    paymentIntentId:{
-        type: String,
+    paymentIntentId: {
+      type: String,
     },
-}, 
- { timeStamps: true }
-)
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Order', OrderSchema)
+module.exports = mongoose.model('Order', OrderSchema);
